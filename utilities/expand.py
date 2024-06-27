fa57f5b1364143f2ac6ef33adb53646b24d8d655
Stephen Mann <steve@mann.ws>
2024-06-27 17:46:34 -0400

    Update expand.py
    Added command line tests

--- utilities/expand.py
+++ utilities/expand.py
@@ -7,6 +7,16 @@ python expand.py inputfile outputfile
 
 import json
 import sys
+import re
+
+
+# Check if a command-line argument has been provided
+if len(sys.argv) < 3 or sys.argv[1] in ['help', '?']:
+    print('Usage: python expand.py inputfile outputfile')
+    sys.exit(1)
+                
+# Get the input file and output file from the command-line arguments
+infile, outfile = sys.argv[1], sys.argv[2]
 
 def format_file(input_file_path, output_file_path):
     with open(input_file_path, 'r') as input_file, open(output_file_path, 'w') as output_file:
@@ -19,9 +29,6 @@ def format_file(input_file_path, output_file_path):
                     line = line.replace("}", "\n}\n\n", 1)
                 line = line.replace(",", ",\n  ")
                 output_file.write(line)
-                
-# Get the input file and output file from the command-line arguments
-infile, outfile = sys.argv[1], sys.argv[2]
 
 # Run the function with the command-line arguments
 format_file(infile, outfile)
