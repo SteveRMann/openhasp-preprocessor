4ed2d4a8748956963d281a1c6082e4884272e2ea
Stephen Mann <steve@mann.ws>
2024-06-28 23:00:17 -0400

    Update utilities

--- utilities/expand.py
+++ utilities/expand.py
@@ -2,7 +2,7 @@
 Expand the jsonl file.
 
 Usage:
-python expand.py inputfile outputfile
+python expand2.py file_path
 """
 
 import sys
@@ -11,29 +11,37 @@ import os
 
 
 # Check if a command-line argument has been provided
-if len(sys.argv) < 3 or sys.argv[1] in ['help', '?']:
-    print('Usage: python expand.py inputfile outputfile')
+if len(sys.argv) < 2 or sys.argv[1] in ['help', '?']:
+    print('Usage: python expand.py file_path')
     sys.exit(1)
                 
-# Get the input file and output file from the command-line arguments
-infile, outfile = sys.argv[1], sys.argv[2]
+# Get the file_path from the command-line arguments
+file_path = sys.argv[1]
 
 # Check if the input file exists
-if not os.path.exists(infile):
-    print(f"Error: The file {infile} does not exist.")
+if not os.path.exists(file_path):
+    print(f"Error: The file {file_path} does not exist.")
     sys.exit(1)
     
-def format_file(input_file_path, output_file_path):
-    with open(input_file_path, 'r') as input_file, open(output_file_path, 'w') as output_file:
-        for line in input_file:
+    
+    
+def expand_file(file_path):
+    # Read the entire file into memory
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    # Open the file in write mode to overwrite it
+    with open(file_path, 'w') as file:
+        for line in lines:
             line = line.strip()
             if line:
                 if line.startswith("{"):
-                    line = line.replace("{", "{\n", 1)
+                    line = line.replace("{", "{\n  ", 1)
                 if line.endswith("}"):
                     line = line.replace("}", "\n}\n\n", 1)
                 line = line.replace(",", ",\n  ")
-                output_file.write(line)
+                file.write(line)
+
 
 # Run the function with the command-line arguments
-format_file(infile, outfile)
+expand_file(file_path)
